5fa939e17388fedef9e4ad3b8fbdc79965431648
Tushar Tailor <tushar.sdmcc.cs@gmail.com>
2026-07-06 12:34:00 +0530

    created basic express server

---
+++ server/server.ts
@@ -0,0 +1,31 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+const app = express();
+
+// Database connection
+await connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Server is Live!");
+});
+
+// Global error handler
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err);
+  res.status(500).send(err?.response?.date?.message || err?.message);
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
