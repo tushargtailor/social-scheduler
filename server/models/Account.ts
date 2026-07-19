0a7eac5d99e192467406bc4b31f97b5d8c7282fc
Tushar Tailor <tushar.sdmcc.cs@gmail.com>
2026-07-19 18:18:02 +0530

    created API for social account creations

---
+++ server/models/Account.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    platform: {
+      type: String,
+      enum: [
+        "twitter",
+        "linkedin",
+        "facebook",
+        "instagram",
+        "facebook_page",
+        "linkedin_page",
+        "instagram_business",
+      ],
+      required: true,
+    },
+    handle: { type: String, required: true },
+    zernioAccountId: { type: String },
+    accessTocken: { type: String },
+    refreshToken: { type: String },
+    tokenExpiresAt: { type: Date },
+    status: {
+      type: String,
+      enum: ["connected", "disconnected"],
+      default: "connected",
+    },
+    avatarUrl: { type: String },
+  },
+  { timestamps: true },
+);
+
+export const Account = mongoose.model("Account", accountSchema);
--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import socialAuthRouter from "./routes/socialAuthRoutes.js";
 
 const app = express();
 
@@ -18,13 +20,14 @@ app.get("/", (_req: Request, res: Response) => {
   res.send("Server is Live!");
 });
 
+app.use("/api/auth", authRouter)
+app.use("/api/oauth", socialAuthRouter)
+
 // Global error handler
 app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
   console.error(err);
   res.status(500).send(err?.response?.date?.message || err?.message);
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
