d3caa9c8769a0c3292e3bac82f09e07089954a00
Tushar Tailor <tushar.sdmcc.cs@gmail.com>
2026-07-30 17:10:21 +0530

    created API for Post

---
+++ server/routes/postRoutes.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { protect } from "../middlewares/authMiddleware.js";
+import {
+  generatePost,
+  getGenerations,
+  getPosts,
+  schedulePost,
+} from "../controllers/postController.js";
+import { upload } from "../config/multer.js";
+
+const postRouter = express.Router();
+
+postRouter.get("/", protect, getPosts);
+postRouter.get("/generations", protect, getGenerations);
+postRouter.post("/", protect, upload.single("media"), schedulePost);
+postRouter.post("/generate", protect, generatePost);
+
+export default postRouter;
--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
 import socialAuthRouter from "./routes/socialAuthRoutes.js";
+import accountRouter from "./routes/accountRoutes.js";
+import postRouter from "./routes/postRoutes.js";
 
 const app = express();
 
@@ -22,6 +24,8 @@ app.get("/", (_req: Request, res: Response) => {
 
 app.use("/api/auth", authRouter)
 app.use("/api/oauth", socialAuthRouter)
+app.use("/api/accounts", accountRouter)
+app.use("/api/posts", postRouter)
 
 // Global error handler
 app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
