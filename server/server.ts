e741b5642d443c3d1b69067b38b1ab4d793271f4
Tushar Tailor <tushar.sdmcc.cs@gmail.com>
2026-07-31 16:14:52 +0530

    backend server completed

--- server/server.ts
+++ server/server.ts
@@ -6,6 +6,8 @@ import authRouter from "./routes/authRoutes.js";
 import socialAuthRouter from "./routes/socialAuthRoutes.js";
 import accountRouter from "./routes/accountRoutes.js";
 import postRouter from "./routes/postRoutes.js";
+import activityRouter from "./routes/activityRoutes.js";
+import { initScheduler } from "./services/schedulerService.js";
 
 const app = express();
 
@@ -26,6 +28,10 @@ app.use("/api/auth", authRouter)
 app.use("/api/oauth", socialAuthRouter)
 app.use("/api/accounts", accountRouter)
 app.use("/api/posts", postRouter)
+app.use("/api/activity", activityRouter)
+
+// Initialize Scheduler
+initScheduler()
 
 // Global error handler
 app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
